96cfc603d10129c7700701c150b414a68e4497a7
AbayoHJovin <abayohirwajovin@gmail.com>
2025-05-20 20:53:24 +0200

    first commit

---
+++ src/main/java/com/shopwise/ShopWiseApplication.java
@@ -0,0 +1,13 @@
+package com.shopwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopWiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopWiseApplication.class, args);
+    }
+
+}
